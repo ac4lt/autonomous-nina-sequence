c66b63166c4444e6f03fe648615dcf44bb62e0bb
ac4lt <linda@ac4lt.org>
2025-04-25 08:17:59 -0700

    SRO specific scripts
    - added the custom shutdown script that the SRO scope uses
    - removed the global SendToDiscord.bat script since it's no longer needed

--- AutonmousScripts/SendToDiscord.bat
+++
@@ -1,2 +0,0 @@
-REM Repllace "<discordwebhook>" with the URL of your Discord webhook
-curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"%*\"}" <discordwebhook>

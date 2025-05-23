f2c9d1dceb9c13be4abd49dccafb4959019c0e07
ac4lt <linda@ac4lt.org>
2025-05-23 10:34:56 -0500

    0.38.0 - power changes
    - moved the call to startup.bat before the custom power on 1 template
    - ceravolo custom power 1 no longer controls the WPS
    - ceravolo custom power off no longer controls the WPS
    - ceravolo startup.bat now controls the WPS
    - ceravolo shutdown.bat now controls the WPS
    
    This is to avoid flakiness in the WPS ASCOM driver that seems to affect the unit with the Ceravolo despite firmware updates.

--- sequence/Observatory Customizations/DSOC_Ceravolo/Scripts/shutdown.bat
+++ sequence/Observatory Customizations/DSOC_Ceravolo/Scripts/shutdown.bat
@@ -2,3 +2,11 @@ REM shutdown all auxiliary apps
 taskkill /im pwi3.exe
 taskkill /im phd2.exe
 taskkill /im astrophysicscommandcenter.exe
+
+REM The Web Power Switch ASCOM driver on the Ceravolo likes to give timeout messages
+REM and while they are benign, it also sometimes just fails for not apparent reason.
+REM Instead, we control it from the command line
+
+curl -u admin:melotte15 http://192.168.0.101:370/outlet?3=OFF
+curl -u admin:melotte15 http://192.168.0.101:370/outlet?4=OFF
+curl -u admin:melotte15 http://192.168.0.101:370/outlet?7=OFF

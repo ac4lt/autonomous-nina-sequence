d59195dbb22c65c81852f1134266f78a859665a2
ac4lt <linda@ac4lt.org>
2025-05-23 22:25:28 -0500

    ceravolo script updates
    - fixed the WPS port numbers that I transcribed incorrectly

--- sequence/Observatory Customizations/DSOC_Ceravolo/Scripts/shutdown.bat
+++ sequence/Observatory Customizations/DSOC_Ceravolo/Scripts/shutdown.bat
@@ -7,6 +7,6 @@ REM The Web Power Switch ASCOM driver on the Ceravolo likes to give timeout mess
 REM and while they are benign, it also sometimes just fails for not apparent reason.
 REM Instead, we control it from the command line
 
-curl -u admin:melotte15 http://192.168.0.101:370/outlet?3=OFF
 curl -u admin:melotte15 http://192.168.0.101:370/outlet?4=OFF
+curl -u admin:melotte15 http://192.168.0.101:370/outlet?5=OFF
 curl -u admin:melotte15 http://192.168.0.101:370/outlet?7=OFF
--- sequence/Observatory Customizations/DSOC_Ceravolo/Scripts/startup.bat
+++ sequence/Observatory Customizations/DSOC_Ceravolo/Scripts/startup.bat
@@ -2,6 +2,6 @@ REM The Web Power Switch ASCOM driver on the Ceravolo likes to give timeout mess
 REM and while they are benign, it also sometimes just fails for not apparent reason.
 REM Instead, we control it from the command line
 
-curl -u admin:melotte15 http://192.168.0.101:370/outlet?3=ON
 curl -u admin:melotte15 http://192.168.0.101:370/outlet?4=ON
+curl -u admin:melotte15 http://192.168.0.101:370/outlet?5=ON
 curl -u admin:melotte15 http://192.168.0.101:370/outlet?7=ON

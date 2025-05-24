7b1b592b5bd8a837a996f21b6be82a28315b6b0f
ac4lt <linda@ac4lt.org>
2025-05-24 09:45:21 -0500

    ceravolo startup.bat wait
    wait 5s after turning on power for USB devices to wake up and connect to the PC

--- sequence/Observatory Customizations/DSOC_Ceravolo/Scripts/startup.bat
+++ sequence/Observatory Customizations/DSOC_Ceravolo/Scripts/startup.bat
@@ -5,3 +5,6 @@ REM Instead, we control it from the command line
 curl -u admin:melotte15 http://192.168.0.101:370/outlet?4=ON
 curl -u admin:melotte15 http://192.168.0.101:370/outlet?5=ON
 curl -u admin:melotte15 http://192.168.0.101:370/outlet?7=ON
+
+REM wait 5 seconds for the USB devices to wake up and get recognized by the PC
+timeout -t 5 /nobreak

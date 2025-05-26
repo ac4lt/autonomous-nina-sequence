9f4b8863ce102fcdcbcba3f7a691c2a25592333c
ac4lt <linda@ac4lt.org>
2025-05-25 22:10:41 -0500

    DSOC_AP305 mount wake changes
    - removed wake mount from custom power on 1 template
    - added wol.exe call to startup.bat to wake up the mount - not wol.exe is not in the github repository and must be seperately downloaded

--- sequence/Observatory Customizations/DSOC_AP305/Scripts/startup.bat
+++ sequence/Observatory Customizations/DSOC_AP305/Scripts/startup.bat
@@ -10,10 +10,10 @@ curl -u admin:melotte15 http://192.168.0.51:365/outlet?7=ON
 curl -u admin:melotte15 http://192.168.0.51:365/outlet?8=ON
 
 REM send a wake-on-lan packet to the 10 Micron mount to boot it.
-REM "C:\Wake On Lan\wol.exe" 00-C0-08-8C-66-FB
+"C:\Wake On Lan\wol.exe" 00-C0-08-8C-66-FB
 
 REM waste a bunch of time pinging the mount to give it time to boot and we can at least see something happening
 REM ping -n 120 192.168.0.52	
 
 REM Wait 5 seconds for the USB devices to wake up and connect to the PC
-timeout -t 5 /nobreak
+timeout -t 120 /nobreak
